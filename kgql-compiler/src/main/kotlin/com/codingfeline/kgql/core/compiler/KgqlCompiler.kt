cba250acd0e9856665eb2aa631150c5d7a728bdd
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2018-12-20 17:21:04 +0900

    :construction:

--- kgql-compiler/src/main/kotlin/com/codingfeline/kgql/core/compiler/KgqlCompiler.kt
+++ kgql-compiler/src/main/kotlin/com/codingfeline/kgql/core/compiler/KgqlCompiler.kt
@@ -1,4 +1,14 @@
 package com.codingfeline.kgql.core.compiler
 
-class KgqlCompiler {
+import java.io.File
+
+private typealias FileAppender = (fileName: String) -> Appendable
+
+object KgqlCompiler {
+
+    fun compile(file: File, output: FileAppender) {
+    }
+
+    fun writeDocumentWrapperFile(sourceFile: File, output: FileAppender) {
+    }
 }
