b5179501fa9cbdea1e5a041be00efb25502f6256
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2019-01-14 20:20:24 +0900

    fix dependency resolution

--- release.sh
+++ release.sh
@@ -3,7 +3,8 @@
 ./gradlew clean
 ./gradlew build
 ./gradlew -Pnative.deploy=true :kgql-core:bintrayUpload
-./gradlew :kgql-compiler:bintrayUpload
+#./gradlew :kgql-compiler:bintrayUpload
+./gradlew :kgql-compiler:uploadArchives
 #./gradlew :kgql-gradle-plugin:bintrayUpload
-./gradlew :kgql-gradle-plugin:publishPlugins
+#./gradlew :kgql-gradle-plugin:publishPlugins
 ./gradlew :kgql-gradle-plugin:uploadArchives
