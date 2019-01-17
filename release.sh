2fffd49dbe719512b8d2dd94e69b7d839d099506
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2019-01-17 18:29:44 +0900

    remove unneeded native.deploy property

--- release.sh
+++ release.sh
@@ -2,9 +2,6 @@
 
 ./gradlew clean
 ./gradlew build
-./gradlew -Pnative.deploy=true :kgql-core:publish
-#./gradlew :kgql-compiler:bintrayUpload
+./gradlew :kgql-core:publish
 ./gradlew :kgql-compiler:uploadArchives
-#./gradlew :kgql-gradle-plugin:bintrayUpload
-#./gradlew :kgql-gradle-plugin:publishPlugins
 ./gradlew :kgql-gradle-plugin:uploadArchives
