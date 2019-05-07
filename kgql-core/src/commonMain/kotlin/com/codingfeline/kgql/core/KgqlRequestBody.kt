2b92aec795cc67e3141a32b72fecaeee0b39ba2e
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2019-05-07 11:48:58 +0900

    :memo: add documents

--- kgql-core/src/commonMain/kotlin/com/codingfeline/kgql/core/KgqlRequestBody.kt
+++ kgql-core/src/commonMain/kotlin/com/codingfeline/kgql/core/KgqlRequestBody.kt
@@ -6,7 +6,19 @@ import kotlinx.serialization.json.JsonObject
  * Root interface for GraphQL request body
  */
 interface KgqlRequestBody {
+    /**
+     * Operation name.
+     * If [query] contains more than two operation, you must specify this. Otherwise can be null.
+     */
     val operationName: String?
+
+    /**
+     * Query document
+     */
     val query: String
+
+    /**
+     * Parameters for [operationName] in [query]
+     */
     val variables: JsonObject?
 }
--- kgql-core/src/commonMain/kotlin/com/codingfeline/kgql/core/internal/KgqlValue.kt
+++ kgql-core/src/commonMain/kotlin/com/codingfeline/kgql/core/internal/KgqlValue.kt
@@ -4,6 +4,19 @@ package com.codingfeline.kgql.core.internal
  * Class to express GraphQL's explicit/implicit null.
  */
 sealed class KgqlValue<out T : Any?> {
-    data class Some<out T : Any?>(val value: T) : KgqlValue<T>()
+    /**
+     * Represents value presence
+     */
+    data class Some<out T : Any?>(
+        /**
+         * Actual value. can be null.
+         * we need the fact that null is intentionally set to distinguish it from implicit null.
+         */
+        val value: T
+    ) : KgqlValue<T>()
+
+    /**
+     * represents value is not set(implicitly null)
+     */
     object None : KgqlValue<Nothing>()
 }
